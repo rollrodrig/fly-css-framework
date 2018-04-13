134f4b98b853406f260143ef2afca9e3efc76c52
rolly <rollrodrig@gmail.com>
2018-04-13 15:19:57 -0500

    removing map

--- README.md
+++
@@ -1,2 +0,0 @@
-# fly-css-framework
-Simple and beautiful css framework to build suits and prototypes on the fly
---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var watch = require('gulp-watch');
+var sass = require('gulp-sass');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var connect       = require('gulp-connect');
+
+var publicPath = './dist/';
+
+
+gulp.task('connect', function(){
+    connect.server({
+        port:9797,
+        root:'./dist'
+    })
+});
+
+
+gulp.task('browserify', function() {
+    return browserify('./src/js/Index.js')
+        .bundle()
+        .pipe(source('main.js'))
+        .pipe(gulp.dest(publicPath+'js'));
+});
+
+
+// Sass
+gulp.task('sass', function () {
+    gulp.src('./src/sass/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest(publicPath+'css'));
+});
+
+
+// Watch
+gulp.task('watch', function () {
+    watch('./src/js/**/*.js', function(event) {
+        gulp.start('browserify');
+    });
+    watch('./src/sass/**/*.scss', function(event) {
+        gulp.start('sass');
+    });
+});
+
+gulp.task('default', ['sass','browserify','watch','connect']);
+ 
+
+
+
+
+
+
+
