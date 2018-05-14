02656066f69f9abab9f0f4f49820f3ba90dca70c
rolly <rollrodrig@gmail.com>
2018-05-14 10:28:31 -0500

    fix tab index

--- dist/js/fly/src/components/Tab.js
+++ dist/js/fly/src/components/Tab.js
@@ -13,9 +13,18 @@
 		this.taBodyitemsName = 'fy-tab-b';
 
 		this.tabActiveClass = '-a';
+		this.tabPreviousIndexOpen = 0;
+		this.tabIndexOpen = 0;
 		this.getElements();
 		this.addHeaderItemsIndex();
 		this.addBodyItemsIndex();
+		this.setCurrentTab();
+	}
+	Tab.prototype.setCurrentTab = function(){
+		var i = this.h.getElementsByClassName(this.tabActiveClass)[0]
+				.getAttribute(this.tabHeaderItemsName)
+		this.tabPreviousIndexOpen = i;
+		this.tabIndexOpen = i;		
 	}
 	Tab.prototype.getElements = function(){
 		this.h = this.tab.getElementsByClassName(this.tabHeaderClass)[0];
@@ -48,14 +57,21 @@
 		tab.classList
 			.add(this.tabActiveClass);
 		var index = tab.getAttribute(this.tabHeaderItemsName);
+		this.tabPreviousIndexOpen = this.tabIndexOpen;
+		this.tabIndexOpen = index;
 		this.openBodyElem(index);
 		// current.classList.add(this.tabActiveClass);
 	}
 	Tab.prototype.openBodyElem = function(index){
-		this.b.getElementsByClassName(this.tabActiveClass)[0]
+		// console.log(this.b.getElementsByClassName(this.tabActiveClass)[0]);
+		// this.b.getElementsByClassName(this.tabActiveClass)[0]
+		// 	.classList
+		// 	.remove(this.tabActiveClass);
+			
+		this.b.querySelector('['+this.taBodyitemsName+'="'+this.tabPreviousIndexOpen+'"]')
 			.classList
 			.remove(this.tabActiveClass);
-		this.b.querySelector('['+this.taBodyitemsName+'="'+index+'"]')
+		this.b.querySelector('['+this.taBodyitemsName+'="'+this.tabIndexOpen+'"]')
 			.classList
 			.add(this.tabActiveClass);
 	}
